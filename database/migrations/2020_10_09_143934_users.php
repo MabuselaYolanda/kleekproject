e0a1dfd8cf38be4da1c508dde25ceca29a333cb6
Tumelo Baloyi <RockHardJim@users.noreply.github.com>
2020-10-09 17:15:18 +0200

    Initial commit

---
+++ database/migrations/2020_10_09_143934_users.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Users extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->string('user', 50)->primary()->unique();
+            $table->string('name');
+            $table->string('surname');
+            $table->enum('role', ['Buyer', 'Business', 'Administrator', 'Potential', 'Employee'])->default('Buyer');
+            $table->string('cellphone', 12)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2020_10_09_144641_profiles.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Profiles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->string('user')->unique();
+            $table->string('avatar');
+            $table->string('address');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
