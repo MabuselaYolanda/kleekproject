f9f5721c9da9f383be6c7647be44c3df7580fe68
Tumelo Baloyi <RockHardJim@users.noreply.github.com>
2020-10-09 20:30:56 +0200

    Added Welcome pages.

--- resources/views/partials/welcome.blade.php
+++ resources/views/partials/welcome.blade.php
@@ -1,1 +1,64 @@
-<?php
+<!DOCTYPE html>
+<html lang="zxx" class="overflow-x-hidden"> <!-- Use overflow-x-hidden class if you use same slider as client section slider -->
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Kleek">
+    <meta name="author" content="Kleek">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Kleek | Business Automation Platform</title>
+    <link rel="stylesheet" href="{{ asset('welcome/assets/css/bootstrap.min.css') }}" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('welcome/assets/css/animate.min.css') }}" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('welcome/assets/css/owl.carousel.min.css') }}" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('welcome/assets/css/owl.theme.default.min.css') }}" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('welcome/assets/css/meanmenu.min.css') }}" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('welcome/assets/css/magnific-popup.min.css') }}" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('welcome/assets/css/boxicons.min.css') }}" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('welcome/assets/css/line-awesome.min.css') }}" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('welcome/assets/css/flaticon.css') }}" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('welcome/assets/css/style.css') }}" type="text/css" media="all" />
+    <link rel="stylesheet" href="{{ asset('welcome/assets/css/responsive.css') }}" type="text/css" media="all" />
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+</head>
+
+
+<body class="overflow-x-hidden">
+<div class="preloader orange-gradient">
+    <div class="preloader-wrapper">
+        <div class="preloader-grid">
+            <div class="preloader-grid-item preloader-grid-item-1"></div>
+            <div class="preloader-grid-item preloader-grid-item-2"></div>
+            <div class="preloader-grid-item preloader-grid-item-3"></div>
+            <div class="preloader-grid-item preloader-grid-item-4"></div>
+            <div class="preloader-grid-item preloader-grid-item-5"></div>
+            <div class="preloader-grid-item preloader-grid-item-6"></div>
+            <div class="preloader-grid-item preloader-grid-item-7"></div>
+            <div class="preloader-grid-item preloader-grid-item-8"></div>
+            <div class="preloader-grid-item preloader-grid-item-9"></div>
+        </div>
+    </div>
+</div>
+@yield('content')
+<footer class="footer-bg">
+    <div class="container">
+        <div class="footer-lower">
+            <div class="footer-lower-item footer-copyright-text">
+                <p>Copyright ©2020 Design & Developed by the <a href="#" target="_blank">Kleek Team</a></p>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="{{ asset('welcome/assets/js/jquery-3.5.1.min.js') }}"></script>
+<script src="{{ asset('welcome/assets/js/popper.min.js') }}"></script>
+<script src="{{ asset('welcome/assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('welcome/assets/js/jquery.magnific-popup.min.js') }}"></script>
+<script src="{{ asset('welcome/assets/js/owl.carousel.min.js') }}"></script>
+<script src="{{ asset('welcome/assets/js/jquery.ajaxchimp.min.js') }}"></script>
+<script src="{{ asset('welcome/assets/js/form-validator.min.js') }}"></script>
+<script src="{{ asset('welcome/assets/js/contact-form-script.js') }}"></script>
+<script src="{{ asset('welcome/assets/js/jquery.meanmenu.min.js') }}"></script>
+<script src="{{ asset('welcome/assets/js/jquery.waypoints.js') }}"></script>
+<script src="{{ asset('welcome/assets/js/counter-up.js') }}"></script>
+<script src="{{ asset('welcome/assets/js/script.js') }}"></script>
+</body>
+</html>
